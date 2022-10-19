608de0a63839ea0ddcba3485143489be327f1d82
bagusdwi30 <warakasfriok123@gmail.com>
2022-10-19 17:29:45 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
 <body>
     <h1>helloworld</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum autem incidunt laboriosam tenetur dolorem consequuntur ad? Rem consectetur sed eligendi ipsum tenetur quasi ipsam id veniam harum reprehenderit. Quos, quaerat!</p>
+    <ul>
+        <li>Anjay</li>
+    </ul>
 </body>
 
 </html>
